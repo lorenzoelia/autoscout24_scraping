3a68fd34854102417ff3bb78528f8013b0b9c751
loren <lorenzo.elia@studenti.units.it>
2023-10-09 14:42:59 +0200

    Added main.py

--- main.py
+++ main.py
@@ -1,11 +1,43 @@
 from AutoScout24Scraper import AutoScout24Scraper
+from DataProcessor import DataProcessor
+from MileagePricePlotter import MileagePricePlotter
+from MileagePriceRegression import MileagePriceRegression
+
 
 if __name__ == "__main__":
     make = "mercedes-benz"
     model = "a-250"
     num_pages = 20
 
-    scraper = AutoScout24Scraper(make, model)
-    scraper.scrape(num_pages)
-    scraper.save_to_csv()
-    scraper.quit_browser()
+    input_file = f'listings_{make}_{model}.csv'
+
+    # scraper = AutoScout24Scraper(make, model)
+    # scraper.scrape(num_pages, True)
+    # scraper.save_to_csv(input_file)
+    # scraper.quit_browser()
+
+    # Data Processing
+    output_file_no_duplicates = 'listings_no_duplicates.csv'
+    output_file_preprocessed = 'listings_preprocessed.csv'
+
+    processor = DataProcessor(input_file)
+    data = processor.read_data()
+    data_no_duplicates = processor.remove_duplicates(data)
+    data_preprocessed = processor.preprocess_data(data_no_duplicates)
+    processor.save_processed_data(data_preprocessed, output_file_preprocessed)
+
+    # Mileage-Price Regression
+    data_preprocessed['mileage'] = data_preprocessed['mileage'].astype(int)
+    data_preprocessed['mileage_grouped'] = (data_preprocessed['mileage'] // 1000) * 1000
+    grouped_data = data_preprocessed.groupby('mileage_grouped')['price'].agg(['mean', 'std']).reset_index()
+    mileage_values = grouped_data['mileage_grouped']
+    average_price_values = grouped_data['mean']
+
+    regression = MileagePriceRegression(mileage_values, average_price_values)
+    degree = 2
+    mileage_values, predicted_prices = regression.perform_regression(degree)
+
+    # Mileage-Price Plotting
+    std_deviation_values = grouped_data['std']
+    plotter = MileagePricePlotter(mileage_values, average_price_values, std_deviation_values)
+    plotter.plot_mileage_price(predicted_prices, degree)
