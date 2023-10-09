03129f9264f76e225bae716b8a021f5e64b149ee
loren <lorenzo.elia@studenti.units.it>
2023-10-09 14:04:51 +0200

    Moved to OOP style

---
+++ DataProcessor.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+
+class DataProcessor:
+    def __init__(self, input_file):
+        self.input_file = input_file
+
+    def read_data(self):
+        return pd.read_csv(self.input_file)
+
+    def remove_duplicates(self, df):
+        columns_to_check_duplicates = ['make', 'model', 'mileage', 'fuel-type', 'first-registration']
+        return df.drop_duplicates(subset=columns_to_check_duplicates, keep='first')
+
+    def preprocess_data(self, df):
+        df.loc[df['first-registration'] == 'new', 'mileage'] = 0
+        return df[df['mileage'] != 'unknown'].reset_index(drop=True)
+
+    def save_processed_data(self, df, output_file):
+        df.to_csv(output_file, index=False)
+        print("Modified data saved to", output_file)
