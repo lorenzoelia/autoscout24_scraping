fe83c1623f6b60f4e8acb889e56d9899863c119f
s257241 <lorenzo.elia@studenti.units.it>
2023-10-18 17:37:59 +0200

    Refactored with scrape option

--- main.py
+++ main.py
@@ -4,54 +4,52 @@ from MileagePricePlotter import MileagePricePlotter
 from MileagePriceRegression import MileagePriceRegression
 from TextFileHandler import TextFileHandler
 
-if __name__ == "__main__":
-    make = "audi"
-    model = "a1"
-    version = ""
-    year_from = "2018"
-    year_to = ""
-    power_from = ""
-    power_to = ""
-    powertype = "kw"
-    num_pages = 20
-    zipr = 100
 
-    zip_list_file_path = 'capoluoghi.csv'
-
+def main(scrape=False):
     handler = TextFileHandler(zip_list_file_path)
     handler.load_data_csv()
-
     zip_list = handler.export_capoluogo_column()
     zip_list = [item.lower() for item in zip_list]
-
     input_file = f'listings/listings_{make}_{model}.csv'
-
-    scraper = AutoScout24Scraper(make, model, version, year_from, year_to, power_from, power_to, powertype, zip_list, zipr)
-    scraper.scrape(num_pages, True)
-    scraper.save_to_csv(input_file)
-    scraper.quit_browser()
-
+    if scrape:
+        scraper = AutoScout24Scraper(make, model, version, year_from, year_to, power_from, power_to, powertype, zip_list,
+                                     zipr)
+        scraper.scrape(num_pages, True)
+        scraper.save_to_csv(input_file)
+        scraper.quit_browser()
     # Data Processing
     output_file_no_duplicates = f'listings/listings_{make}_{model}_no_duplicates.csv'
     output_file_preprocessed = f'listings/listings_{make}_{model}_preprocessed.csv'
-
     processor = DataProcessor(input_file)
     data = processor.read_data()
     data_no_duplicates = processor.remove_duplicates(data)
     data_preprocessed = processor.preprocess_data(data_no_duplicates)
     data_rounded = processor.round(data_preprocessed, 1000)
-    processor.save_processed_data(data_preprocessed, output_file_preprocessed)
-
+    processor.save_processed_data(data_rounded, output_file_preprocessed)
     # Mileage-Price Regression
     grouped_data = data_preprocessed.groupby('mileage_grouped')['price'].agg(['mean', 'std']).reset_index()
     mileage_values = grouped_data['mileage_grouped']
     average_price_values = grouped_data['mean']
+    regression = MileagePriceRegression(mileage_values, average_price_values)
+    mileage_values, predicted_prices, best_degree = regression.perform_regression()
+    # Mileage-Price Plotting
+    std_deviation_values = grouped_data['std']
+    plotter = MileagePricePlotter(mileage_values, average_price_values, std_deviation_values)
+    plotter.plot_mileage_price(predicted_prices, best_degree)
 
-    for degree in zipr(1, 5):
-        regression = MileagePriceRegression(mileage_values, average_price_values)
-        mileage_values, predicted_prices = regression.perform_regression(degree)
 
-        # Mileage-Price Plotting
-        std_deviation_values = grouped_data['std']
-        plotter = MileagePricePlotter(mileage_values, average_price_values, std_deviation_values)
-        plotter.plot_mileage_price(predicted_prices, degree)
+if __name__ == "__main__":
+    make = "ford"
+    model = "fiesta"
+    version = "st"
+    year_from = ""
+    year_to = ""
+    power_from = ""
+    power_to = ""
+    powertype = "kw"
+    num_pages = 10
+    zipr = 100
+
+    zip_list_file_path = 'capoluoghi.csv'
+
+    main()
